68836757def66c73a5b7bd16a13bd6f095539e3c
Buldes <115708157+Buldes@users.noreply.github.com>
2023-11-03 13:09:27 +0100

    adding google analytics #3

--- src/App.js
+++ src/App.js
@@ -5,8 +5,6 @@ import { BankBalanceLableUdate } from './Functions/UpdateKomponentsUpArear';
 import { Laptop, Mobile, Tablet } from './Components/Appjs-Components';
 import { GetAspectRatio } from './Functions/sreen';
 import { setMobileDevice } from './Data/bool';
-import { Helmet } from 'react-helmet';
-
 
 function App() {
   useEffect(() => { 
@@ -14,70 +12,14 @@ function App() {
     BankBalanceLableUdate()});
   
     Initialization()
-    
+
   if (GetAspectRatio() <= 1) {
     setMobileDevice(true)
-    return (
-      <div>
-
-        <Helmet>
-          <script 
-            async 
-            src="https://www.googletagmanager.com/gtag/js?id=G-BDSBMQWQ9R"/>
-          <script 
-            dangerouslySetInnerHTML={{ __html:`
-              window.dataLayer = window.dataLayer || [];
-              function gtag(){dataLayer.push(arguments);}
-              gtag('js', new Date());
-                
-              gtag('config', 'G-BDSBMQWQ9R');`}}/>
-        </Helmet>
-
-        <Mobile/>
-      </div>
-      )
+    return <Mobile/>
   }
-  else if (GetAspectRatio() <= 1.7) {
-    return (
-      <div>
-        
-        <Helmet>
-          <script 
-            async 
-            src="https://www.googletagmanager.com/gtag/js?id=G-BDSBMQWQ9R"/>
-          <script 
-            dangerouslySetInnerHTML={{ __html:`
-              window.dataLayer = window.dataLayer || [];
-              function gtag(){dataLayer.push(arguments);}
-              gtag('js', new Date());
-                
-              gtag('config', 'G-BDSBMQWQ9R');`}}/>
-        </Helmet>
-
-        <Tablet/>
-      </div>
-      )
-  }
+  else if (GetAspectRatio() <= 1.7) return <Tablet/>
   else{ 
-    return (
-      <div>
-        
-        <Helmet>
-          <script 
-            async 
-            src="https://www.googletagmanager.com/gtag/js?id=G-BDSBMQWQ9R"/>
-          <script 
-            dangerouslySetInnerHTML={{ __html:`
-              window.dataLayer = window.dataLayer || [];
-              function gtag(){dataLayer.push(arguments);}
-              gtag('js', new Date());
-                
-              gtag('config', 'G-BDSBMQWQ9R');`}}/>
-        </Helmet>
-
-        <Laptop/>
-      </div>
-      )
+    return <Laptop/> 
   }
 }
 
